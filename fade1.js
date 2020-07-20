fca1a22c8d0c5c81b833235c3a26c88afad9ba9c
sannanphan <55860922+sannanphan@users.noreply.github.com>
2020-07-19 17:39:19 -0700

    added data

--- fade1.js
+++ fade1.js
@@ -5,10 +5,19 @@ $("html").mousemove(function( event ) {
     $("div2").fadeIn(3900); 
 });    
 */
-$("div2").hide();
-$("div2").delay(3000).fadeIn(3900); 
-$(function () {
-  $(document).scroll(function () {
-  $("div2").delay(3000).fadeIn(3900); 
-  });
-});
+
+if ($(window).width() > 480) {
+    $("div2").hide();
+    var fade_in = false;
+    /*
+    $(document).scroll(function () {
+        var fade_in = true;
+    });
+    */
+    $("html").mousemove(function(){
+        var fade_in = true;
+    });
+    if (fade_in = true){
+        $("div2").fadeIn(3000); 
+    }
+}
--- fadeout.js
+++ fadeout.js
@@ -5,10 +5,17 @@ $("nav").show();
 $("html").mousemove(function( event ){
     $("nav").fadeOut(2300);
 }); */
+
 $("nav").show();
-$("nav").fadeOut(4000);
-$(function () {
-  $(document).scroll(function () {
-  $("nav").fadeOut(4000);
-  });
+var fade = false;
+$(document).scroll(function() {
+    var fade = true;
 });
+$("html").mousemove(function(){
+    var fade = true;
+});
+
+//$("nav").fadeOut(4000);
+if (fade = true) {
+  $("nav").fadeOut(3000);
+}
