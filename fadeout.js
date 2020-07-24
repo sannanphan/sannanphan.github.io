0af87eabca3f4d045271510fa0419f8f51fa4cdb
sannanphan <55860922+sannanphan@users.noreply.github.com>
2020-07-23 20:15:04 -0700

    upd

--- fadeout.js
+++ fadeout.js
@@ -9,10 +9,10 @@ $("html").mousemove(function( event ){
 
 $("nav").show();
 window.onscroll = function () { 
-  $("nav").fadeOut(2500);
+  $("nav").fadeOut(2000);
 };
 
 
 $("html").mousemove(function(){
-  $("nav").fadeOut(2500);
+  $("nav").fadeOut(2000);
 });
